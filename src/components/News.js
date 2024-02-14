4a76c69699f4ba43d131e9ab1aa785d30ad3a17b
SHIVAM KUMAR JHA <shivamjha082001@gmail.com>
2024-02-14 21:27:00 +0530

    updated

---
+++ src/components/News.js
@@ -0,0 +1,90 @@
+import React, { useEffect, useState } from 'react'
+import NewsItem from './NewsItem'
+import Spinner from './Spinner';
+import PropTypes from 'prop-types'
+import InfiniteScroll from "react-infinite-scroll-component";
+
+
+const News = (props) => {
+  const [articles, setArticles] = useState([])
+  const [loading, setloading] = useState(true)
+  const [page, setpage] = useState(1)
+  const [totalResults, settotalResults] = useState(0)
+
+
+  const capitalizeFirstLetter = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+
+  const updateNews = async () => {
+    props.setProgress(10);
+    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page}&pagesize=${props.pageSize}`;
+    setloading(true);
+    let data = await fetch(url);
+    props.setProgress(30);
+    let parsedData = await data.json()
+    props.setProgress(70);
+    setArticles(parsedData.articles);
+    settotalResults(parsedData.totalResults);
+    setloading(false);
+    props.setProgress(100);
+
+  }
+
+  useEffect(() => {
+    document.title = `NewsFusion - ${capitalizeFirstLetter(props.category)}`;
+    updateNews();
+    // eslint-disable-next-line
+  }, [])
+
+  const fetchMoreData = async () => {
+    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page + 1}&pagesize=${props.pageSize}`;
+    setpage(page + 1);
+    let data = await fetch(url);
+    let parsedData = await data.json()
+    setArticles(articles.concat(parsedData.articles));
+    settotalResults(parsedData.totalResults);
+    setloading(false);
+
+  };
+
+
+  return (
+    <>
+      <h1 className='text-center' style={{ margin: "35px 0px", marginTop: "90px" }}>NewsFusion - Top {capitalizeFirstLetter(props.category)} Headlines</h1>
+      {loading && <Spinner />}
+      <InfiniteScroll
+        dataLength={articles.length}
+        next={fetchMoreData}
+        hasMore={articles.length !== totalResults}
+        loader={<Spinner />}
+      >
+        <div className="container">
+          <div className="row">
+            {articles.map((element) => (
+              <div className="col-md-4" key={element.url}>
+                <NewsItem key={element.url} title={element.title ? element.title.slice(0, 40) : ""} description={element.description ? element.description.slice(0, 88) : ""} imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt} source={element.source.name} />
+              </div>
+            ))}
+          </div>
+        </div>
+      </InfiniteScroll>
+    </>
+  )
+
+}
+
+
+News.defaultProps = {
+  country: 'in',
+  pageSize: 9,
+  category: "general"
+}
+News.propTypes = {
+  country: PropTypes.string,
+  pageSize: PropTypes.number,
+  category: PropTypes.string
+};
+
+export default News
